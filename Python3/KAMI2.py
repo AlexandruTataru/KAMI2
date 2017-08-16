1560b4bfd4c08678fd9a53b4350aeee96be4284c
Tataru Alexandru Flavian <alexandruflavian@gmail.com>
2017-08-16 18:10:33 +0300

    Added code to extract the neighbors of a triangle and also do a search on all triangles contained inside a single color.

--- Python3/KAMI2.py
+++ Python3/KAMI2.py
@@ -3,6 +3,7 @@ from pynput import keyboard
 from enum import Enum
 import math
 import random
+from multiprocessing import Queue
 
 class ORIENTATION(Enum):
     LEFT = 1
@@ -22,6 +23,33 @@ print('1. Color red\n2. Color green\n3. Color blue\n4. Color yellow\nu. Undraw\n
 BOARD_STARTING_COLOR_SELECTION = '3'
 
 currentAction = BOARD_STARTING_COLOR_SELECTION
+
+triangles = []
+
+def getNeighbors(triangle):
+    print('a1');
+    print(triangle.getInternalColor())
+    centerPoint = triangle.getCenterPoint()
+    print('a2');
+    n1 = centerPoint
+    n2 = centerPoint
+    n3 = centerPoint
+    print('a3');
+    if triangle.getOrientation() == ORIENTATION.LEFT:
+        n1 = Point(centerPoint.x + 2 * (1.0/3.0 * move_along_x), centerPoint.y)
+        n2 = Point(centerPoint.x - 1.0/3.0 * move_along_x, centerPoint.y + TRIANGLE_SIZE/2)
+        n3 = Point(centerPoint.x - 1.0/3.0 * move_along_x, centerPoint.y - TRIANGLE_SIZE/2)
+    elif triangle.getOrientation() == ORIENTATION.RIGHT:
+        n1 = Point(centerPoint.x + 1.0/3.0 * move_along_x, centerPoint.y - TRIANGLE_SIZE/2)
+        n2 = Point(centerPoint.x + 1.0/3.0 * move_along_x, centerPoint.y + TRIANGLE_SIZE/2)
+        n3 = Point(centerPoint.x - 2 * (1.0/3.0 * move_along_x), centerPoint.y)
+
+    neighbors = []
+    for neighboardTriangle in triangles:
+        if neighboardTriangle.isPointInside(n1) or neighboardTriangle.isPointInside(n2) or neighboardTriangle.isPointInside(n3):
+                neighbors.append(neighboardTriangle)
+
+    return neighbors
 
 def getColor():
     rand_shade = random.randrange(30, 100, 20)
@@ -57,6 +85,9 @@ class Kami2Triangle:
     def draw(self, window):
         self.triangle.draw(window)
 
+    def undraw(self):
+        self.triangle.undraw()
+
     def isPointInside(self, pt):
         b1 = sign(pt, self.p1, self.p2) < 0.0;
         b2 = sign(pt, self.p2, self.p3) < 0.0;
@@ -70,32 +101,54 @@ class Kami2Triangle:
     def setInternalColor(self, color):
         self.color = color
 
+    def getInternalColor(self):
+        return self.color
+
     def getOrientation(self):
+        print('o1')
         if self.p2.x > self.p1.x:
+            print('o2')
             return ORIENTATION.RIGHT
         elif self.p2.x < self.p1.x:
+            print('o3')
             return ORIENTATION.LEFT
-        
+        print('o4')
         return ORIENTATION.UNKNOWN
 
     def getCenterPoint(self):
+        print('c1')
         x = 0
         y = self.p1.y + TRIANGLE_SIZE/2
-        if triangle.getOrientation() == ORIENTATION.LEFT:
+        print('c2')
+        if self.getOrientation() == ORIENTATION.LEFT:
+            print('c3')
             x = self.p1.x - 1.0/3.0 * move_along_x
-        elif triangle.getOrientation() == ORIENTATION.RIGHT:
+        elif self.getOrientation() == ORIENTATION.RIGHT:
+            print('c4')
             x = self.p1.x + 1.0/3.0 * move_along_x
+        print('c5')
         return Point(x, y)
 
 def startToProcessTheBoard():
     print('Board has been processed')
-    return
+
+    for triangle in triangles:
+        if triangle.hasBeenMarked == False:
+            internalColor = triangle.getInternalColor()
+            q = [triangle]
+            while len(q) > 0:
+                currTriangle = q.pop()
+                currTriangle.setArtisticColor(color_rgb(255, 255, 255))
+                if currTriangle.hasBeenMarked == False:
+                    neighbors = getNeighbors(currTriangle)
+                    for neighbor in neighbors:
+                        if internalColor == neighbor.getInternalColor():
+                            q.append(neighbor)
+                    currTriangle.hasBeenMarked = True
 
 def on_press(key):
     global currentAction
     currentAction = key.char
-    if currentAction == 's':
-        startToProcessTheBoard()
 
 lis = keyboard.Listener(on_press=on_press)
 lis.start()
@@ -123,8 +176,6 @@ def drawLeftTriangle(sp):
 
     return triangle
 
-triangles = []
-
 startPosY = 0
 while startPosY < BOARD_SIZE_Y - TRIANGLE_SIZE:
     startPosX = 0
@@ -142,31 +193,14 @@ while startPosY < BOARD_SIZE_Y - TRIANGLE_SIZE:
 
 while True:
     clickedPoint = win.getMouse()
-    for triangle in triangles:
-        if triangle.isPointInside(clickedPoint):
-            if currentAction == 'u':
-                triangles.remove(triangle)
-                triangle.undraw()
-            else:
-                triangle.setArtisticColor(getColor())
-                triangle.setInternalColor(currentAction)
-                centerPoint = triangle.getCenterPoint()
-                n1 = centerPoint
-                n2 = centerPoint
-                n3 = centerPoint
-                if triangle.getOrientation() == ORIENTATION.LEFT:
-                    n1 = Point(centerPoint.x + 2 * (1.0/3.0 * move_along_x), centerPoint.y)
-                    n2 = Point(centerPoint.x - 1.0/3.0 * move_along_x, centerPoint.y + TRIANGLE_SIZE/2)
-                    n3 = Point(centerPoint.x - 1.0/3.0 * move_along_x, centerPoint.y - TRIANGLE_SIZE/2)
-                elif triangle.getOrientation() == ORIENTATION.RIGHT:
-                    n1 = Point(centerPoint.x + 1.0/3.0 * move_along_x, centerPoint.y - TRIANGLE_SIZE/2)
-                    n2 = Point(centerPoint.x + 1.0/3.0 * move_along_x, centerPoint.y + TRIANGLE_SIZE/2)
-                    n3 = Point(centerPoint.x - 2 * (1.0/3.0 * move_along_x), centerPoint.y)
-
-                for neighboardTriangle in triangles:
-                    if neighboardTriangle.isPointInside(n1):
-                        neighboardTriangle.setArtisticColor(color_rgb(255, 255, 255))
-                    elif neighboardTriangle.isPointInside(n2):
-                        neighboardTriangle.setArtisticColor(color_rgb(255, 255, 255))
-                    elif neighboardTriangle.isPointInside(n3):
-                        neighboardTriangle.setArtisticColor(color_rgb(255, 255, 255))
+    if currentAction == 's':
+        startToProcessTheBoard()
+    else:
+        for triangle in triangles:
+            if triangle.isPointInside(clickedPoint):
+                if currentAction == 'u':
+                    triangles.remove(triangle)
+                    triangle.undraw()
+                else:
+                    triangle.setArtisticColor(getColor())
+                    triangle.setInternalColor(currentAction)
