9d9dd71aac7a75937a29bcfa6956c97b47d2848b
Tataru Alexandru Flavian <alexandruflavian@gmail.com>
2017-08-20 17:09:15 +0300

    Improved current color sepection, added UI button and color pickers, removed text based selection, improved clearing algorithm

--- Python3/KAMI2.py
+++ Python3/KAMI2.py
@@ -9,78 +9,59 @@ class ORIENTATION(Enum):
     RIGHT = 2
     UNKNOWN = 3
 
+class ACTION(Enum):
+    COLOR = 1
+    CLEAR = 2
+    UNDRAW = 3
+    PROCESS = 4
+
+currentAction = ACTION.COLOR
+
 # Board set-up parameters
 TRIANGLE_SIZE = 70
+COLOR_PALLETE_SIZE = 105
 NR_TRIANGLES_HORIZONTAL = 10
 NR_TRIANGLES_VERTICAL = 14
 
-TEXT_AREA_WIDTH = 200
-CHAR_WIDTH = 5
-
 move_along_x = math.sqrt(3)/2.0 * TRIANGLE_SIZE
-BOARD_SIZE_X = move_along_x * NR_TRIANGLES_HORIZONTAL + TEXT_AREA_WIDTH + 1
+BOARD_SIZE_X = move_along_x * NR_TRIANGLES_HORIZONTAL + COLOR_PALLETE_SIZE * 2
 BOARD_SIZE_Y = TRIANGLE_SIZE * NR_TRIANGLES_VERTICAL
-FOLDING_COLOR = color_rgb(50, 50, 50)
-OUTPUT_FILE = "C:\\Users\\alexandruflavian.ta\\Desktop\\output.txt"
+FOLDING_COLOR = color_rgb(70, 70, 70)
+OUTPUT_FILE = "C:\\Users\\atataru\\Desktop\\output.txt"
 
 window = GraphWin("KAMI2", BOARD_SIZE_X, BOARD_SIZE_Y)
 
-def addLeftAllignedText(text, y):
-    size = len(text)
-    offset = TEXT_AREA_WIDTH/2 - size * CHAR_WIDTH
-    instruction = Text(Point(TEXT_AREA_WIDTH/2 - offset + 5, y), text)
-    instruction.setFace('courier')
-    instruction.setWidth(TEXT_AREA_WIDTH)
-    instruction.draw(window)
+names = ['DarkRed',
+         'Yallow',
+         'Blue',
+         'PaperGrey',
+         'Orange',
+         'DarkBlue',
+         'Pink',
+         'LightBlue',
+         'DarkGrey',
+         'Burgundy']
 
-scanline = 20
-addLeftAllignedText("Available colors:", scanline)
-scanline += 20
-addLeftAllignedText("0. Dark Red", scanline)
-scanline += 20
-addLeftAllignedText("1. Yellow", scanline)
-scanline += 20
-addLeftAllignedText("2. Blue", scanline)
-scanline += 20
-addLeftAllignedText("3. Paper Grey", scanline)
-scanline += 20
-addLeftAllignedText("4. Orange", scanline)
-scanline += 20
-addLeftAllignedText("5. Dark Blue", scanline)
-scanline += 20
-addLeftAllignedText("6. Pink", scanline)
-scanline += 20
-addLeftAllignedText("7. Light Blue", scanline)
-scanline += 20
-addLeftAllignedText("8. Dark Grey", scanline)
-scanline += 20
-addLeftAllignedText("9. Burgundy", scanline)
-scanline += 20
-addLeftAllignedText("", scanline)
-scanline += 20
-addLeftAllignedText("Available actions:", scanline)
-scanline += 20
-addLeftAllignedText("u. Undraw triangle", scanline)
-scanline += 20
-addLeftAllignedText("c. Clear drawing", scanline)
-scanline += 20
-addLeftAllignedText("s. Output graph", scanline)
-scanline += 20
+colors = [  color_rgb(192, 68, 56),
+            color_rgb(196, 148, 66),
+            color_rgb(51, 112, 113),
+            color_rgb(212, 202, 175),
+            color_rgb(210, 107, 63),
+            color_rgb(36, 65, 70),
+            color_rgb(157, 51, 73),
+            color_rgb(157, 232, 195),
+            color_rgb(102, 51, 0),
+            color_rgb(101, 28, 48)
+        ]
 
-groupIdx = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+colorDictionary = {}
+for idx in range(0, len(names)):
+    colorDictionary[names[idx]] = colors[idx]
 
-BOARD_STARTING_COLOR_SELECTION = '0'
-
-currentAction = BOARD_STARTING_COLOR_SELECTION
-
-def on_press(key):
-    global currentAction
-    currentAction = key.char
-
-lis = keyboard.Listener(on_press=on_press)
-lis.start()
+currentColor = colors[3]
 
 triangles = []
+uiButtons = []
 
 def getNeighbors(triangle):
     centerPoint = triangle.getCenterPoint()
@@ -104,25 +85,58 @@ def getNeighbors(triangle):
 
     return neighbors
 
-currentColor = None
-
 def getColor():
     global currentColor
-    if currentAction in ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']:
-        colors = [  color_rgb(192, 68, 56),   # Dark Red
-                    color_rgb(196, 148, 66),  # Yellow
-                    color_rgb(51, 112, 113),  # Blue
-                    color_rgb(212, 202, 175), # Paper Grey
-                    color_rgb(210, 107, 63),  # Orange
-                    color_rgb(36, 65, 70),    # Dark Blue
-                    color_rgb(157, 51, 73),   # Pink
-                    color_rgb(157, 232, 195), # Light Blue
-                    color_rgb(102, 51, 0),    # Dark Grey
-                    color_rgb(101, 28, 48)    # Burgundy
-                ]
-        currentColor = colors[int(currentAction)]
-
     return currentColor
+
+class Kami2Button:
+    def __init__(self, p, width, height, text, action):
+        self.action = action
+        self.p1 = p
+        self.p2 = Point(p.x + width, p.y)
+        self.p3 = Point(p.x + width, p.y + height)
+        self.p4 = Point(p.x, p.y + height)
+        vertices = [self.p1, self.p2, self.p3, self.p4]
+        self.square = Polygon(vertices)
+        self.square.setOutline(color_rgb(0, 0, 0));
+
+        self.label = Text(Point(p.x + width/2, p.y + height / 2), text)
+        self.label.setFace('courier')
+        self.label.setSize(16)
+        self.label.setWidth(width)
+
+    def draw(self, window):
+        self.square.draw(window)
+        self.label.draw(window)
+
+    def getAction(self):
+        return self.action
+
+    def isPointInside(self, pt):
+        isHor = (pt.x >= self.p1.x and pt.x <= self.p2.x)
+        isVer = (pt.y >= self.p2.y and  pt.y <= self.p3.y)
+        return isHor and isVer;
+
+class Kami2PalleteChooser:
+    def __init__(self, p, size, color):
+        self.p1 = p
+        self.p2 = Point(p.x + size, p.y)
+        self.p3 = Point(p.x + size, p.y + size)
+        self.p4 = Point(p.x, p.y + size)
+        vertices = [self.p1, self.p2, self.p3, self.p4]
+        self.square = Polygon(vertices)
+        self.color = color
+        self.square.setFill(color)
+        self.square.setOutline(color_rgb(0, 0, 0))
+        self.square.setWidth(1)
+
+    def draw(self, window):
+        self.square.draw(window)
+
+    def isPointInside(self, pt):
+        isHor = (pt.x >= self.p1.x and pt.x <= self.p2.x)
+        isVer = (pt.y >= self.p2.y and  pt.y <= self.p3.y)
+        return isHor and isVer;
 
 class Kami2Triangle:
 
@@ -136,7 +150,7 @@ class Kami2Triangle:
         self.hasBeenMarked = False
         self.color = color
 
-        self.triangle.setFill(getColor()) # artistic color
+        self.triangle.setFill(color) # artistic color
         self.triangle.setOutline(FOLDING_COLOR)
         self.triangle.setWidth(1)
 
@@ -144,8 +158,10 @@ class Kami2Triangle:
         self.label.setSize(5)
 
     def getColorGroup(self):
-        colorGroups = ['DarkRed', 'Yellow', 'Blue', 'PaperGrey', 'Orange', 'DarkBlue', 'Pink', 'LightBlue', 'DarkGrey', 'Burgundy']
-        return colorGroups[int(self.color)]
+        for idx in range(0, len(colors)):
+            if(colors[idx] == self.color):
+                return names[idx]
+        return None
 
     def draw(self, window):
         self.triangle.draw(window)
@@ -162,13 +178,11 @@ class Kami2Triangle:
 
         return ((b1 == b2) and (b2 == b3));
 
-    def setArtisticColor(self, color):
+    def setColor(self, color):
+        self.color = color
         self.triangle.setFill(color)
 
-    def setInternalColor(self, color):
-        self.color = color
-
-    def getInternalColor(self):
+    def getColor(self):
         return self.color
 
     def getOrientation(self):
@@ -188,8 +202,13 @@ class Kami2Triangle:
         return Point(x, y)
 
     def displayBelongingGroup(self):
-        #self.label.setText(self.group[len(self.group) - 1])
-        self.label.setText('P1')
+        self.label.setText(self.group[0] + self.group[len(self.group) - 1])
+
+    def clearTriangle(self):
+        self.color = getColor()
+        self.triangle.setFill(self.color)
+        self.label.setText('')
+        self.hasBeenMarked = False
 
 availableConnections = []
 
@@ -203,12 +222,16 @@ def writeDataToFile(data):
     file.write(data)
     file.close()
 
+groupIdx = {}
+for color in names:
+    groupIdx[color] = 0
+
 def startToProcessTheBoard():
     for triangle in triangles:
         if triangle.hasBeenMarked == False:
-            internalColor = triangle.getInternalColor()
-            groupIdx[int(internalColor)] = groupIdx[int(internalColor)] + 1
-            groupId = triangle.getColorGroup() + str(groupIdx[int(internalColor)])
+            internalColor = triangle.getColor()
+            groupIdx[triangle.getColorGroup()] += 1
+            groupId = triangle.getColorGroup() + str(groupIdx[triangle.getColorGroup()])
             triangle.group = groupId
             q = [triangle]
             while len(q) > 0:
@@ -219,7 +242,7 @@ def startToProcessTheBoard():
                 if currTriangle.hasBeenMarked == False:
                     neighbors = getNeighbors(currTriangle)
                     for neighbor in neighbors:
-                        if internalColor == neighbor.getInternalColor():
+                        if internalColor == neighbor.getColor():
                             q.append(neighbor)
                         elif neighbor.hasBeenMarked == True and neighbor.group != None:
                             makeConnection(currTriangle.group, neighbor.group)
@@ -241,59 +264,102 @@ def drawTriangle(startingPoint, orientation):
         p2 = Point(startingPoint.x - move_along_x, startingPoint.y + TRIANGLE_SIZE/2)
     p3 = Point(startingPoint.x, startingPoint.y + TRIANGLE_SIZE)
 
-    triangle = Kami2Triangle(p1, p2, p3, currentAction)
+    triangle = Kami2Triangle(p1, p2, p3, getColor())
     triangle.draw(window)
     
     return triangle
 
-def clearBoardAndRedrawEverything():
-    global groupIdx
-    for i in range(0, len(groupIdx)):
-        groupIdx[i] = 0
-
-    global currentAction
-    currentAction = BOARD_STARTING_COLOR_SELECTION
-
-    for triangle in triangles:
-        triangle.undraw()
-
+def drawBoardForFirstTime():
     startPosY = 0
-    headerPosX = TEXT_AREA_WIDTH
-    while headerPosX <= BOARD_SIZE_X - move_along_x * 2:
+    headerPosX = 0
+    while headerPosX <= BOARD_SIZE_X - move_along_x * 2 - COLOR_PALLETE_SIZE * 2:
         triangles.append(drawTriangle(Point(headerPosX, startPosY - TRIANGLE_SIZE/2), ORIENTATION.RIGHT))
         triangles.append(drawTriangle(Point(headerPosX + move_along_x * 2, startPosY - TRIANGLE_SIZE/2), ORIENTATION.LEFT))
         headerPosX += move_along_x * 2
 
     while startPosY <= BOARD_SIZE_Y - TRIANGLE_SIZE:
-        startPosX = move_along_x + TEXT_AREA_WIDTH
-        while startPosX <= BOARD_SIZE_X - move_along_x:
+        startPosX = move_along_x
+        while startPosX <= BOARD_SIZE_X - move_along_x - COLOR_PALLETE_SIZE * 2:
             triangles.append(drawTriangle(Point(startPosX, startPosY), ORIENTATION.LEFT))
             triangles.append(drawTriangle(Point(startPosX, startPosY), ORIENTATION.RIGHT))
             startPosX += move_along_x * 2
-        startPosX = TEXT_AREA_WIDTH
-        while startPosX <= BOARD_SIZE_X - move_along_x * 2:
+        startPosX = 0
+        while startPosX <= BOARD_SIZE_X - move_along_x * 2 - COLOR_PALLETE_SIZE * 2:
             triangles.append(drawTriangle(Point(startPosX, startPosY + TRIANGLE_SIZE/2), ORIENTATION.RIGHT))
             triangles.append(drawTriangle(Point(startPosX + move_along_x * 2, startPosY + TRIANGLE_SIZE/2), ORIENTATION.LEFT))
             startPosX += move_along_x * 2
         startPosY += TRIANGLE_SIZE
 
+def clearBoard():
+    global groupIdx
+    for color in names:
+        groupIdx[color] = 0
+
+    global currentAction
+    currentAction = ACTION.COLOR
+
+    for triangle in triangles:
+        triangle.clearTriangle()
+
+palleteChooserZones = []
+
+def setCurrentActionAsUndraw():
+    global currentAction
+    currentAction = ACTION.UNDRAW
+
 def executionLoop():
     while True:
         clickedPoint = window.getMouse()
-        if currentAction == 's':
-            startToProcessTheBoard()
-        if currentAction == 'c':
-            clearBoardAndRedrawEverything()
-        else:
-            for triangle in triangles:
-                if triangle.isPointInside(clickedPoint):
-                    if currentAction == 'u':
-                        triangles.remove(triangle)
-                        triangle.undraw()
-                    else:
-                        triangle.setArtisticColor(getColor())
-                        triangle.setInternalColor(currentAction)
+        for button in uiButtons:
+            if button.isPointInside(clickedPoint):
+                if button.getAction() == ACTION.CLEAR:
+                    clearBoard()
+                elif button.getAction() == ACTION.PROCESS:
+                    startToProcessTheBoard()
+                elif button.getAction() == ACTION.UNDRAW:
+                    setCurrentActionAsUndraw()
+        for pallete in palleteChooserZones:
+            if pallete.isPointInside(clickedPoint):
+                global currentColor
+                currentColor = pallete.color
+                global currentAction
+                currentAction = ACTION.COLOR
+        for triangle in triangles:
+            if triangle.isPointInside(clickedPoint):
+                if currentAction == ACTION.UNDRAW:
+                    triangles.remove(triangle)
+                    triangle.undraw()
+                else:
+                    triangle.setColor(getColor())
+
+def drawColorPalleteUI():
+    colorPalleteX = BOARD_SIZE_X - COLOR_PALLETE_SIZE * 2
+    colorPalleteY = 0
+
+    for colorIdx in range(0, len(colors)):
+        zone = Kami2PalleteChooser(Point(colorPalleteX, colorPalleteY), COLOR_PALLETE_SIZE, colors[colorIdx])
+        zone.draw(window)
+        palleteChooserZones.append(zone)
+        colorPalleteX += COLOR_PALLETE_SIZE
+        if colorPalleteX == BOARD_SIZE_X:
+            colorPalleteX -= COLOR_PALLETE_SIZE * 2
+            colorPalleteY += COLOR_PALLETE_SIZE
+
+def drawMenuButtonsUI():
+    BUTTON_WIDTH = COLOR_PALLETE_SIZE * 2 - 10
+    BUTTON_HEIGHT = 50
+    button = Kami2Button(Point(BOARD_SIZE_X - COLOR_PALLETE_SIZE * 2 + 5, BOARD_SIZE_Y - 50), BUTTON_WIDTH, BUTTON_HEIGHT, 'Process', ACTION.PROCESS)
+    uiButtons.append(button)
+    button.draw(window)
+    button = Kami2Button(Point(BOARD_SIZE_X - COLOR_PALLETE_SIZE * 2 + 5, BOARD_SIZE_Y - 55 - BUTTON_HEIGHT), BUTTON_WIDTH, BUTTON_HEIGHT, 'Clear', ACTION.CLEAR)
+    uiButtons.append(button)
+    button.draw(window)
+    button = Kami2Button(Point(BOARD_SIZE_X - COLOR_PALLETE_SIZE * 2 + 5, BOARD_SIZE_Y - 60 - BUTTON_HEIGHT * 2), BUTTON_WIDTH, BUTTON_HEIGHT, 'Undraw', ACTION.UNDRAW)
+    uiButtons.append(button)
+    button.draw(window)
 
 if __name__ == "__main__":
-    clearBoardAndRedrawEverything()
+    drawColorPalleteUI()
+    drawMenuButtonsUI()
+    drawBoardForFirstTime()
     executionLoop()
